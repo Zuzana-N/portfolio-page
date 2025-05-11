b970caf01c399968ab1ed46723829066e23fc64d
Zuzana-N <122560006+Zuzana-N@users.noreply.github.com>
2025-05-11 19:23:51 +0200

    updated stack and my work

--- index.js
+++ index.js
@@ -1,26 +1,35 @@
-const primaryHeader = document.querySelector(".primary-header");
-const navToggle = document.querySelector(".mobile-nav-toggle");
-const primaryNav = document.querySelector(".primary-nav");
-const navList = document.querySelector(".nav-list");
+const primaryHeader = document.querySelector('.primary-header')
+const navToggle = document.querySelector('.mobile-nav-toggle')
+const primaryNav = document.querySelector('.primary-nav')
+const navList = document.querySelector('.nav-list')
 
-
-navToggle.addEventListener("click", () => {
-    primaryNav.hasAttribute("data-visible") ? navToggle.setAttribute("aria-expanded", false) : navToggle.setAttribute("aria-expanded", true);
-    primaryNav.toggleAttribute("data-visible");
-    primaryHeader.toggleAttribute("data-overlay");
+navToggle.addEventListener('click', () => {
+	primaryNav.hasAttribute('data-visible')
+		? navToggle.setAttribute('aria-expanded', false)
+		: navToggle.setAttribute('aria-expanded', true)
+	primaryNav.toggleAttribute('data-visible')
+	primaryHeader.toggleAttribute('data-overlay')
 })
-navList.addEventListener("click", () => {
-    primaryNav.hasAttribute("data-visible") ? navToggle.setAttribute("aria-expanded", false) : navToggle.setAttribute("aria-expanded", true);
-    primaryNav.toggleAttribute("data-visible");
-    primaryHeader.toggleAttribute("data-overlay");
+navList.addEventListener('click', () => {
+	primaryNav.hasAttribute('data-visible')
+		? navToggle.setAttribute('aria-expanded', false)
+		: navToggle.setAttribute('aria-expanded', true)
+	primaryNav.toggleAttribute('data-visible')
+	primaryHeader.toggleAttribute('data-overlay')
 })
 
-const emailIcon = document.getElementById("email-icon");
-emailIcon.addEventListener("click", function() {
-    navigator.clipboard.writeText("info@zuzana-n.cz");
-});
+const emailIcon = document.getElementById('email-icon')
+emailIcon.addEventListener('click', function () {
+	navigator.clipboard.writeText('info@zuzana-n.cz')
+})
 
-const phoneIcon = document.getElementById("phone-icon");
-phoneIcon.addEventListener("click", function() {
-    navigator.clipboard.writeText("+420604828515");
-});
+const phoneIcon = document.getElementById('phone-icon')
+phoneIcon.addEventListener('click', function () {
+	navigator.clipboard.writeText('+420604828515')
+})
+
+const copyrightYear = document.querySelector('.copyright-year')
+if (copyrightYear) {
+	const currentYear = new Date().getFullYear()
+	copyrightYear.textContent = currentYear
+}
